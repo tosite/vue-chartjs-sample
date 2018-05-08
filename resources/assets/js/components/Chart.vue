ebd8b54292313afabe084392b7a3674ba6b46bb8
tosite <tl091264@gmail.com>
2018-05-08 16:34:14 +0900

    add doughnut-graph

--- resources/assets/js/components/Chart.vue
+++ resources/assets/js/components/Chart.vue
@@ -1,19 +1,109 @@
+<template>
+  <div class="small">
+    <h1>vue-chartjs</h1>
+    <div class="half">
+      <doughnut-chart :chart-data="doughnutcollection"></doughnut-chart>
+    </div>
+    <div class="half">
+      <line-chart :chart-data="datacollection"></line-chart>
+    </div>
+    <div>
+      <input type="text" v-model="lines">
+      <button @click="fillData()">Repaint</button>
+      <button @click="randomize()">Randomize</button>
+    </div>
+  </div>
+</template>
+
 <script>
-import { Bar } from 'vue-chartjs'
+import LineChart from   '../chart/LineChart.js'
+import DoughnutChart from '../chart/DoughnutChart.js'
 
 export default {
-    extends: Bar,
-    mounted () {
-        // Overwriting base render method with actual data.
-        this.renderChart({
-            labels: ['January', 'February'],
-                datasets: [{
-                    label: 'GitHub Commits',
-                    backgroundColor: '#f87979',
-                    data: [40, 20]
-                }
-            ]
-        })
+  components: {
+    LineChart,
+    DoughnutChart,
+
+  },
+  data () {
+    return {
+      datacollection: null,
+      doughnutcollection: null,
+      lines: 3,
     }
+  },
+  mounted () {
+    this.fillData()
+  },
+  methods: {
+    fillData () {
+      var datasets = []
+      var dDatasets = []
+      var labels = []
+      var val = 0
+      var dLabels = []
+      var dColors = []
+      var min = 0
+      var max = 0
+      var loopCnt = this.lines
+      for (var i = 0; i < loopCnt;i++) {
+        labels = [this.getRandomInt(), this.getRandomInt()]
+
+        if (min > labels[0]) {
+          min = labels[0]
+        }
+        if (min > labels[1]) {
+          min = labels[0]
+        }
+        if (max < labels[0]) {
+          max = labels[0]
+        }
+        if (max < labels[1]) {
+          max = labels[1]
+        }
+
+        datasets.push({
+          label: 'data-' + (i + 1),
+          backgroundColor: 'rgba(255,100,100,0.1)',
+          data: labels,
+        })
+
+        dLabels.push('data-' + (i + 1))
+        dDatasets.push(this.getRandomInt())
+        var code = i * 20
+        dColors.push('rgba(255,'+code+','+code+',0.4)')
+      }
+      this.datacollection = {
+        labels: [min, max],
+        datasets: datasets
+      }
+      this.doughnutcollection = {
+        labels: dLabels,
+        datasets: [
+        {
+          data: dDatasets,
+          backgroundColor: dColors
+        }
+        ]
+      }
+    },
+    randomize () {
+      this.lines = Math.floor(Math.random() * 10) + 1;
+      this.fillData();
+    },
+    getRandomInt () {
+      return Math.floor(Math.random() * (50 - 5 + 1)) + 5
+    }
+  }
 }
 </script>
+
+<style>
+.small {
+  margin:  100px auto;
+}
+.half {
+  width: 45%;
+  display: inline-block;
+}
+</style>
--- resources/assets/js/routes.js
+++ resources/assets/js/routes.js
@@ -1,8 +1,8 @@
 import VueRouter from 'vue-router';
 console.log('initialized routes.');
 const routes = [
-    { path: '/', name: 'index', component: require('./components/Index.vue') },
-    { path: '/chart', name: 'chart', component: require('./components/Chart.vue') },
+    // { path: '/', name: 'index', component: require('./components/Index.vue') },
+    { path: '/', name: 'chart', component: require('./components/Chart.vue') },
 ];
 
 export default new VueRouter({
